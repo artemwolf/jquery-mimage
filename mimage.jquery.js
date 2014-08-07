977c2933beea89e79bcd4a7a2181899b1237b79b
artemwolf <wolf@nw-lab.com>
2014-08-07 15:41:44 +0400

    Create mimage.jquery.js

---
+++ mimage.jquery.js
@@ -0,0 +1,30 @@
+{
+    "name": "mimage",
+    "title": "jQuery Multisource Image",
+    "description": "jQuery plugin for mediaquery-like using of multiple images sources. Short & clear implementation of the idea of the <picture> element.",
+    "keywords": [
+        "images",
+        "responsive",
+        "adaptive",
+        "media-queries"
+    ],
+    "version": "0.1.0",
+    "author": {
+        "name": "Artem Wolf",
+        "email": "wolf@nw-lab.com",
+        "url": "https://about.me/artemwolf"
+    },
+    "licenses": [
+        {
+            "type": "MIT",
+            "url": "https://github.com/artemwolf/jquery-mimage/blob/master/LICENSE"
+        }
+    ],
+    "bugs": "https://github.com/artemwolf/jquery-mimage/issues",
+    "homepage": "http://artemwolf.github.io/jquery-mimage",
+    "docs": "https://github.com/artemwolf/jquery-mimage",
+    "download": "http://artemwolf.github.io/jquery-mimage",
+    "dependencies": {
+        "jquery": ">=1.5"
+    }
+}
